02a2d32b3c09371889682ad0bbdb7072c3cba1de
Yoga Baskoro <baskoroyoga40@gmail.com>
2021-12-28 12:13:42 +0700

    Setting up express and dotenv

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("OK");
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+	console.log(`Server running on PORT ${PORT} in ${process.env.NODE_ENV}`);
+});
